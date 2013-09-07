e46fa3e2219df576dc0d1471c3b03b9a2591a475
Juliana Gomez <juligomez17@gmail.com>
2013-09-07 02:13:20 -0400

    Basic login UI.

---
+++ ReceiptMe/src/main/java/com/example/receiptme/Home.java
@@ -0,0 +1,19 @@
+package com.example.receiptme;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+/**
+ *
+ * This is where we will use the Dropbox API to  login.
+ */
+public class Home extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+
+}
--- ReceiptMe/src/main/java/com/example/receiptme/MainActivity.java
+++ ReceiptMe/src/main/java/com/example/receiptme/MainActivity.java
@@ -3,13 +3,15 @@ package com.example.receiptme;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
     }
 
 
@@ -18,6 +20,15 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    /**
+     * Called when the user selects the login button.
+     * This is where we will use the Dropbox API to login.
+     */
+    public void login(View view){
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
     }
     
 }
